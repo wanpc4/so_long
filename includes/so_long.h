81c1281608dc06ac496f5a9384a9b82c7d45d0d5
Wan Ilhan Idriss Wan Ab Kadir <wwan-ab-@u90z05s02.42kl.edu.my>
2024-09-26 07:57:22 +0800

    Add Makefile and put C libraries

--- includes/so_long.h
+++ includes/so_long.h
@@ -3,15 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wwan-ab- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: wwan-ab- <wwan-ab-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 11:12:27 by wwan-ab-          #+#    #+#             */
-/*   Updated: 2024/09/18 11:13:53 by wwan-ab-         ###   ########.fr       */
+/*   Updated: 2024/09/26 07:53:16 by wwan-ab-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
 #include "../minilibx-lib/mlx.h"
 
 struct color_grade{
